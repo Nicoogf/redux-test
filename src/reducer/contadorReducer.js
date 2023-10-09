f9041e799a25115b6873d0cc66b9405a0cd2fd41
Nicoogf <nicolasgfalabella@gmail.com>
2023-10-09 09:19:42 -0300

    Practica de reducer

--- src/reducer/contadorReducer.js
+++ src/reducer/contadorReducer.js
@@ -2,44 +2,39 @@ const estadoInicial = {
     contador : 0 
 }
 
-
-// El action recibe el tipo y el Payload
-// En el action va a llegar la accion  "SUMAR_UNO"
-
-export default function contadorReducer ( estado = estadoInicial , accion ) {
-    switch (accion.type) {
-        case "SUMAR_UNO" : {
-            return {
-                ...estado ,
-                constador :  estado.contador + 1
-            }
+function elReducer ( estado = estadoInicial , accion ) {
+ switch  ( estado.type ) {
+    case "SUMAR_UNO" : {
+        return {
+            ...estado ,
+            contador : estado.contador + 1  
         }
-        case "RESTAR_UNO" :{
-            return{
-                ...estado ,
-                contador: estado.contador - 1 
-            }
-        }
-        case "SUMAR_CINCO" : {
-            return{
-                ...estado,
-                contador : estado.contador + accion.payload
-            }
-        }
-        case "RESTAR_CINCO" :{
-            return{
-                ...estado,
-                contador :  estado.contador - accion.payload
-            }
-        }
-        case "RESET" : {
-            return{
-                ...estado,
-                contador : 0
-            }
-        }
-         default :
-         return estate
-         
     }
+    case "SUMAR_CINCO" : {
+        return{
+            ...estado,
+            contador : estado.contador + accion.payload
+        }
+    }
+    case "RESTAR_UNO"  :{
+        return{
+            ...estado ,
+            contador: estado.contador - 1
+        }
+    }
+    case "RESUTAR_CINCO" : {
+        return{
+            ...estado ,
+            contador :estado.contador - accion.payload
+        }
+    }
+    case "RESET" : {
+        return{
+            ...estado,
+            contador : 0
+        }
+    }
+    default:
+        return estado
+ }
 }
